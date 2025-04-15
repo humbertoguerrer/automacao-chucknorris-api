5ce4e547f1ae10c40a9fdd8d6a7d5376521e4ad0
Humberto Guerrer Neto <humberto.guerrer@gmail.com>
2025-04-15 17:07:53 -0300

    Segundo e terceiro cenários de teste

--- cypress/e2e/apiChuckNorris.cy.js
+++ cypress/e2e/apiChuckNorris.cy.js
@@ -1,13 +1,39 @@
 // cypress/e2e/apiChuckNorris.cy.js
 
-describe('Chuck Norris API', () => {
-    it('Deve retornar uma lista de categorias', () => {
+describe('Testes da API Chuck Norris', () => {
+
+    let categorias = [];
+
+    it('Lista as categorias disponíveis', () => {
         cy.request('https://api.chucknorris.io/jokes/categories').then((res) => {
             expect(res.status).to.eq(200);
             expect(res.body).to.be.an('array');
             expect(res.body).not.be.empty;
-        })
 
-        
+        })
+    })
+
+    it('Retorna uma piada para cada categoria válida', () => {
+
+        cy.request('https://api.chucknorris.io/jokes/categories').then((categoriaRes) => {
+            const categorias = categoriaRes.body;
+            categorias.forEach((categoria) => {
+                cy.request(`https://api.chucknorris.io/jokes/random?category=${categoria}`).then((res) => {
+                    expect(res.status).to.eq(200);
+                    expect(res.body).to.have.property('value');
+                    expect(res.body.value).not.be.empty;
+                    expect(res.body.categories).to.include(categoria);
+                })
+            })
+        })
+    })
+
+    it('Retorna erro 404 para uma categoria inválida', () => {
+        cy.request({
+            url: 'https://api.chucknorris.io/jokes/random?category=inexistente',
+            failOnStatusCode: false
+        }).then((res) => {
+            expect(res.status).to.eq(404)
+        })
     })
 })
